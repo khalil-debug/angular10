8bcb9d6938ee09e72f09d3de4a97a8e85f002511
khalil <khaliltrigu@gmail.com>
2021-12-16 04:51:53 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './general/general.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { EnseignantComponent } from './enseignant/enseignant.component';
+import { AuthentificationComponent } from './general/authentification/authentification.component';
+import { RegisterEnseignantComponent } from './general/register-enseignant/register-enseignant.component';
+import { AProposComponent } from './general/a-propos/a-propos.component';
+import { SharedService} from './shared.service';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent,
+    EtudiantComponent,
+    EnseignantComponent,
+    AuthentificationComponent,
+    RegisterEnseignantComponent,
+    AProposComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
